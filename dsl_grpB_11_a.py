daf0157adc33e893bc038a69aeb3ddbe22e9d829
Akshat Mathur <akshatmathur84@gmail.com>
2024-10-20 17:07:43 +0530

    Update dsl_grpB_11_a.py

--- dsl_grpB_11_a.py
+++ dsl_grpB_11_a.py
@@ -3,112 +3,51 @@ a) Write a pythonprogram to store roll numbers of student in array who attended 
 Write function for searching whether particular student attended training program or not, 
 using Linear search and Sentinel search.
 """
-def menu():
-    print("Enter 1 for linear search without recursion.\nEnter 2 for linear search with recursion.\nEnter 3 for Sentinel search")
+def linear_search(rollno,key):
+    #key=int(input("Enter the element to be searched: "))
+    for i in range(len(rollno)):
+        if rollno[i] == key:
+            print("Element found at index")
+        else:
+            print("Element not found")
+
+def sentinel_search(rollno,key):
+    #key=int(input("Enter the element to be searched: "))
+    n=len(rollno)
+    if n == 0:
+        return False
     
-list_of_roll = []
-strength = int(input("Enter the strength of the class - "))
-while strength<1:
-    print("Plss enter a valid strength of class.")
-    strength = int(input("Enter the strength of the class - "))
+    last=rollno[-1]
+    rollno[-1]=key
 
+    i=0
+    while rollno[i]!= key:
+        i+=1
+    rollno[-1]=last
 
-def getroll(size):
-    array = []
-    assign = 0
-    while assign < size:
-        roll_no = int(input("Enter roll numbers of students - "))
-        if roll_no in array:
-            print("Roll no already in list\n")
-            size += 1
-        else:
-            array.append(roll_no)
-        assign += 1
-    return array
-
-recursive_tracker = 0
-
-
-
-
-def LinearSearchWithoutRecursion(case, end , recursive_tracker):
-    for compare in range(0, len(case), 1):
-        if end == case[compare]:
-            return compare
-            break
+    if i<n-1 or rollno[i]==key:
+        print("Element found at index",i)
     else:
-        return -1
+        print("Element not found")
 
+rollno=[]
+n=int(input("Enter the number of students :"))
+for i in range(0,n):
+    ele=int(input("Enter the roll number of the students: "))
+    rollno.append(ele)
 
+flag=1
+while flag==1:
+    print("1.Linear search")
+    print("2.Sentinel search")
+    print("3.Exit")
+    ch=int(input("Enter the choice: "))
+    if ch==1:
+        key=int(input("Enter the element to be searched: "))
+        linear_search(rollno,key)
+    if ch==2:
+        key=int(input("Enter the element to be searched: "))
+        sentinel_search(rollno,key)
+    if ch==3:
+        flag=0
 
-
-def LinearSearchWithRecursion(case_rec, end_rec, recursive_tracker):
-        if recursive_tracker == len(case_rec):
-            return -1
-        if end_rec == case_rec[recursive_tracker]:
-            return recursive_tracker
-        else:
-            return LinearSearchWithRecursion(case_rec, end_rec, recursive_tracker + 1)
-
-
-
-
-def SentinelSearch(sen_array, stock, demand):
-    assume = sen_array[stock - 1]
-    sen_array[stock - 1] = demand
-    supply = 0
-    while sen_array[supply]!=demand:
-           supply += 1
-    sen_array[stock - 1]  = assume
-    if (supply<(stock-1)) or assume == demand:
-        return supply
-    else:
-        return -1
-
-
-
-
-def consequence(function):
-    linear_result = function(list_of_roll, key, recursive_tracker)
-    if linear_result != -1:
-        print("Student is present at",linear_result+1,"location in list of roll number.")
-    else:
-        print("Student is not present")
-
-
-
-
-list_of_roll = getroll(strength)
-key = int(input("Enter the roll no. to be searched -  "))
-menu()
-choice = int(input("Enter choice - "))
-while choice not in (1, 2, 3):
-    print("Pls enter appropriate choice - \n")
-    choice = int(input("Enter choice - "))
-check = True
-while check:
-    if choice == 1:
-        consequence(LinearSearchWithoutRecursion)
-
-    elif choice == 2:
-        consequence(LinearSearchWithRecursion)
-    else:
-        search_result = SentinelSearch(list_of_roll,strength,key)
-        if search_result == -1:
-            print("Key isn't present.")
-        else:
-            print("Key present at",search_result+1,"location of the list of roll numbers.")
-
-    to_continue = input("Want to continue?\nyes or no - ")
-    while (to_continue.lower() not in ("yes", "no")):
-        print("ENTER ONLY YES OR NO\n")
-        to_continue = input("Want to continue?\nyes or no - ")
-
-    if to_continue.lower() == "no":
-        print("\nThnx for using our program.")
-        check = False
-    else:
-        choice = int(input("Enter choice - "))
-        while choice not in (1, 2, 3):
-            print("Pls enter appropriate choice - \n")
-            choice = int(input("Enter choice - "))
