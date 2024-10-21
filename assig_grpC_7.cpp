99f67ba4bd6861ee45c6bbf546d5aa551c225d3c
Akshat Mathur <akshatmathur84@gmail.com>
2024-10-21 13:52:39 +0530

    Update and rename assig_grpC_19.cpp to assig_grpC_7.cpp

--- assig_grpC_7.cpp
+++ assig_grpC_7.cpp
@@ -1,3 +1,4 @@
+*/
 Department of Computer Engineering has student's club named 'Pinnacle Club'. Students 
 of second, third and final year of department can be granted membership on request. 
 Similarly one may cancel the membership of club. First node is reserved for president of 
@@ -8,7 +9,8 @@ a) Add and delete the members as well as president or even secretary.
 b) Compute total number of members of club
 c) Display members
 d) Two linked lists exists for two divisions. Concatenate two lists.
-
+/*
+    
 #include<iostream>
 using namespace std;
 
