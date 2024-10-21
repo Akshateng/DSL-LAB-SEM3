8365254a798756e37681cf4a55eb32546ab30a2e
Akshat Mathur <akshatmathur84@gmail.com>
2024-10-21 13:53:21 +0530

    Update and rename assig_grpC_20.cpp to assig_grpC_8.cpp

--- assig_grpC_8.cpp
+++ assig_grpC_8.cpp
@@ -1,4 +1,4 @@
-'''
+*/
   The ticket booking system of Cinemax theater has to be implemented using C++ program. 
 There are 10 rows and 7 seats in each row. Doubly circular linked list has to be 
 maintained to keep track of free seats at rows. Assume some random booking to start 
@@ -6,7 +6,8 @@ with. Use array to store pointers (Head pointer) to each row. On demand
 a) The list of available seats is to be displayed
 b) The seats are to be booked
 c) The booking can be cancelled.
-'''
+/*
+
 #include <iostream> 
 #include <stdlib.h> 
 using namespace std; 
