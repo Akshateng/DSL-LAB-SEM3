02155cac500c2d7bb1ca60452a97ae4a94df767e
Akshat Mathur <akshatmathur84@gmail.com>
2024-12-11 13:26:48 +0530

    Update assig_grpD_10.cpp

--- assig_grpD_10.cpp
+++ assig_grpD_10.cpp
@@ -5,135 +5,74 @@ parenthesized or not.
 */
 
 
-  
-#include <iostream>
-#define max 10;
+#include<iostream>
 using namespace std;
-class Stack
-{
- public:.
- int top;
- char stack1[];
 
- Stack()
- {
- top=-1;
- }
- void push(char);
- char pop();
- int isfull();
- int isempty();
+class Stack{
+    private:
+    char *arr;
+    int top;
+    int capacity;
+    public:
+    Stack(int size){
+        capacity=size;
+        arr=new char[capacity];
+        top=-1;
+    }
+    ~Stack(){
+        delete[] arr;
+    }
+    bool isfull(){
+        return top==capacity-1;
+    }
+    bool isempty(){
+        return top==-1;
+    }
+    void push(char c){
+        if(isfull()){
+            cout<<"Stack overflow"<<endl;
+            return;
+        }
+        arr[++top]=c;
+    }
+    char pop(){
+        if(isempty()){
+            cout<<"Stack underflow"<<endl;
+            return '\0';
+        }
+        return arr[top--];
+    }
+    bool iswellparenthesised(const string &expression){
+        Stack s(expression.length());
+
+        for (int i=0;i<expression.length();i++){
+            char ch=expression[i];
+            if (ch=='(' || ch=='{' || ch=='['){
+                s.push(ch);
+            }
+            else if (ch==')' || ch=='}' || ch==']'){
+                if (s.isempty()){
+                    return false;
+                }
+                char top=s.pop();
+                if((ch==')' && top!='(') || (ch=='}' && top!='{') || (ch==']' && top!='[')){
+                    return false;
+                }
+            } 
+        }
+        return s.isempty();
+    }
 };
-void Stack::push(char x)
-{
- top=top+1;
- stack1[top]=x;
-}
-char Stack::pop()
-{
- char s;
- s=stack1[top];
- top=top-1;
- return s;
-}
-int Stack::isfull()
-{
- if(top==9)
 
- return 1;
- else
- return 0;
-}
-int Stack::isempty()
-{
- if(top==-1)
- return 1;
- else
- return 0;
-}
-int main()
-{
- Stack s1;
- char exp[20],ch;
- int i=0,ch1;
- cout<<"\n\nParentheses checker.....!!!!!"<<endl;
- cout<<"\nEnter the expression to check whether it is in well form or not: \n"<<endl;
- cin>>exp;
- for(i=0; i<20; i++)
- {
- if ((exp[i]=='(')||(exp[i]=='{')||(exp[i]=='['))
- {
- break;
- }
-
- else if ((exp[i]==')')||(exp[i]=='}')||(exp[i]==']'))
- {
- cout<<"\n\nInvalid expression !!!"<<endl;
- cout<<"No opening bracket found !!!"<<endl;
- //break;
- return 0;
- }
- }
- while(exp[i]!='\0')
- {
- ch=exp[i];
- switch(ch)
- {
- case '(':
- s1.push(ch);break;
- case '[':
-
- s1.push(ch);break;
- case '{':
- s1.push(ch);break;
- case ')':
- if(s1.stack1[s1.top]=='(')
- {
- s1.pop();
- }
- else
- {
- cout<<"The expression is invalid !!!"<<endl;
- cout<<"Reason: Wrong placement of the opening round bracket!!"<<endl;
- return 0;
- }
- break;
- case ']':
- if(s1.stack1[s1.top]=='[')
- {
- s1.pop();
- }
- else
- {
- cout<<"The expression is invalid !!!"<<endl;
- cout<<"Reason: Wrong placement of the opening square bracket!!"<<endl;
- return 0;
- }
- break;
- case '}':
- if(s1.stack1[s1.top]=='{')
- {
- s1.pop();
- }
- else
- {
- cout<<"The expression is invalid !!!"<<endl;
- cout<<"Reason: Wrong placement of the opening curly bracket!!"<<endl;
- return 0;
- }
- break;
- }
- i=i+1;
- }
- if (s1.isempty())
- {
- cout<<"\nThe expression is well parenthesised...\n";
- }
-
- else
- {
- cout<<"\nSorry! Invalid expression or not well parenthesised...\n";
- }
- return 0;
-
+int main(){
+    string expression;
+    cout<<"Enter the expression for checking if it is well parenthesised or not: ";
+    getline(cin, expression);
+    Stack s(expression.length());
+    if(s.iswellparenthesised(expression)){
+        cout<<"Well parenthesised expression"<<endl;
+    }else{
+        cout<<"Not well parenthesised expression"<<endl;
+    }
+    return 0;
 }
